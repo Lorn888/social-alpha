1c159f5279fa61b5ae26c6d60307338b60002df2
Patryk <patrykokpara@hotmail.com>
2023-05-02 22:56:16 +0100

    Added some fonts

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
+import React from 'react';
+
 export default function App() {
+  const headingStyle = {
+    fontFamily: 'Inter, sans-serif',
+    fontWeight: 600,
+    fontSize: '2rem',
+    color: '#333',
+  };
+
   return (
-    <h1 className="text-3xl font-bold underline">
+    <h1 style={headingStyle}>
       Hello world!
     </h1>
-  )
+  );
 }
