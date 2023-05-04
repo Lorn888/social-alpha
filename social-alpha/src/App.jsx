1f5e84438c3a972a71997ef19c3e44e5a5e6a4c2
Patryk <patrykokpara@hotmail.com>
2023-05-04 16:27:20 +0100

    Styling: blur balls

--- social-alpha/src/App.jsx
+++ social-alpha/src/App.jsx
@@ -1,7 +1,8 @@
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline font-display">
-      Hello world!
-    </h1>
+    <div className="App">
+      <div class = "absolute top-[-6%] right-0 w-80 h-56 rounded-full bg-[#a6ddf0] blur-3xl"></div>
+      <div class = "absolute top-36 left-[-8rem] w-80 h-56 rounded-full bg-[#a6ddf0] blur-3xl"></div>
+    </div>
   )
-}
+} 
