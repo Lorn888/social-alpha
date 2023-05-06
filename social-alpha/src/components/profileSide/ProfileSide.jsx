e2da344b622f197b2328d3376b33b1396a400d4c
Patryk <patrykokpara@hotmail.com>
2023-05-06 14:22:29 +0100

    Added styling to the profile side

--- social-alpha/src/components/profileSide/ProfileSide.jsx
+++ social-alpha/src/components/profileSide/ProfileSide.jsx
@@ -3,7 +3,7 @@ import LogoSearch from '../logoSearch/LogoSearch'
 
 const ProfileSide = () => {
   return (
-<div className="ProfileSide w-[18rem]">
+<div className="ProfileSide w-[18rem] flex-col gap-[1rem] items-center overflow-auto">
     <LogoSearch/>
 	</div>  )
 }
