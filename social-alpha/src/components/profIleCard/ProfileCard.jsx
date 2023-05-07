03f227f17094551ea1a2894cf68f1ee6f9065a7f
Patryk <patrykokpara@hotmail.com>
2023-05-07 19:49:16 +0100

    Added Follow status

--- social-alpha/src/components/profIleCard/ProfileCard.jsx
+++ social-alpha/src/components/profIleCard/ProfileCard.jsx
@@ -10,9 +10,26 @@ const ProfileCard = () => {
         <img
           src={profilePic}
           alt="profilePic"
-          className="w-[6rem] rounded-[50%] absolute bottom-[-3rem]"
+          className="w-[6rem] rounded-[50%] absolute bottom-[-3rem] shadow-2xl "
         />
       </div>
+      <div className="ProfileName flex flex-col items-center mt-[3rem] gap-[10px]">
+        <span className="font-bold font-display">Patryk O</span>
+        <span>Junior Dev</span>
+      </div>
+      <div className="followStatus">
+        <hr />
+        <div className="follow">
+          <span>6,890</span>
+          <span>Follwoing</span>
+        </div>
+        <div className="vl"></div>
+        <div className="follow">
+          <span>1</span>
+          <span>Follwers</span>
+        </div>
+      </div>
+      <hr />
     </div>
   );
 };
