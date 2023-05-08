5626765dd2836b9a05beeba8fafb8e8dea374b89
Patryk <patrykokpara@hotmail.com>
2023-05-08 15:55:00 +0100

    Displayed fake followers in the card

--- social-alpha/src/components/followersCard/FollowersCard.jsx
+++ social-alpha/src/components/followersCard/FollowersCard.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import { Followers } from '../Data/FollowersData'
 
 const FollowersCard = () => {
   return (
-    <div className="FollowesCar">
+    <div className="FollowesCard">
         <h3 className="">Who is follwoing you?</h3>
+
+        {Followers.map((follower, id)=> {
+          return(
+            <div className="follower">Yes Follower</div>
+          )
+        })}
     </div>
   )
 }
