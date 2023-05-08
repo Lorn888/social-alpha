a1a789be68571a384936fc0e54832313d870598b
Patryk <patrykokpara@hotmail.com>
2023-05-08 15:44:53 +0100

    Created some fake followers data

---
+++ social-alpha/src/components/Data/FollowersData.js
@@ -0,0 +1,19 @@
+import img1 from "../../img/IMG_1.png"
+import img2 from "../../img/IMG_2.png"
+import img3 from "../../img/IMG_3.png"
+import img4 from "../../img/IMG_4.png"
+
+export const Followers = [
+    {
+        name: "Andrew Thomas", username: "AndrewThomas", img: "img1"
+    },
+    {
+        name: "Sophie Martin", username: "SophieMartin", img: "img2"
+    },
+    {
+        name: "David Lee", username: "DavidLee", img: "img3"
+    },
+    {
+        name: "Emily Chen", username: "EmilyChen", img: "img4"
+    }
+]
