2f59fc176b9704aeab1a22af511591e5c9a4d158
Patryk <patrykokpara@hotmail.com>
2023-05-08 23:44:49 +0100

    Finished followers card

--- social-alpha/src/components/followersCard/FollowersCard.jsx
+++ social-alpha/src/components/followersCard/FollowersCard.jsx
@@ -4,12 +4,12 @@ import { Followers } from "../Data/FollowersData";
 const FollowersCard = () => {
   return (
     <div className="FollowesCard w-full rounded-[0.7rem] gap-[1rem] flex flex-col text-[13px]">
-      <h3 className="">Who is follwoing you?</h3>
+      <h3 className="">Who is following you?</h3>
 
       {Followers.map((follower, id) => {
         return (
           <div className="follower flex justify-between items-center">
-            <div className="flex  gap-[10px]">
+            <div className="flex gap-[10px]">
               <img
                 src={follower.img}
                 alt="Follower img"
@@ -17,11 +17,11 @@ const FollowersCard = () => {
               />
 
               <div className="name flex flex-col items-start justify-center">
-                <span className=" font-bold">{follower.name}</span>
+                <span className="font-bold">{follower.name}</span>
                 <span>@{follower.username}</span>
               </div>
             </div>
-            <button>
+            <button className="h-[2rem] px-[20px] text-white bg-[orange] flex items-center justify-center rounded-[0.5rem] hover:cursor-pointer hover:text-[#fca61f] hover:bg-transparent hover:border-[2px] hover:border-[#fca61f]">
               Follow
             </button>
           </div>
