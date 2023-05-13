acf6f3610eb150aec65d7101af429fecc8787e26
Patryk <patrykokpara@hotmail.com>
2023-05-13 16:15:08 +0100

    Added all post elements

--- social-alpha/src/components/posts/Post.jsx
+++ social-alpha/src/components/posts/Post.jsx
@@ -1,14 +1,29 @@
 import React from "react";
-
+import Comment from "../../img/comment.png";
+import Share from "../../img/share.png";
+import Heart from "../../img/like.png";
+import NotLike from "../../img/notlike.png";
 const Post = ({ data }) => {
   return (
-    <div className="Pos">
-      <img src={data.img} />
+    <div className="Post flex flex-col p-[1rem] bg-[rgba(255,255,255,0.64)] rounded-[1rem] gap-[1rem]">
+      <img src={data.img} className="w-[100%] max-h-[20rem] object-cover rounded-[0.5rem]"/> 
 
-      <div className="postReact">
-        <img src="" alt="" />
-        <img src="" alt="" />
-        <img src="" alt="" />
+      <div className="postReact ">
+        <img src={data.liked ? Heart : NotLike} />
+        <img src={Comment} />
+        <img src={Share} />
+      </div>
+
+      <span>{data.likes} likes</span>
+
+      <div className="details">
+        <span>
+          <b>{data.name} </b>
+        </span>
+
+      <span>
+        {data.desc}
+      </span>
       </div>
     </div>
   );
