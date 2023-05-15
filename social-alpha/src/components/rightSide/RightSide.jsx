c07911c220014cd88ed92d610aa4f8b209557817
Patryk <patrykokpara@hotmail.com>
2023-05-15 16:49:55 +0100

    Added icons to the right side

--- social-alpha/src/components/rightSide/RightSide.jsx
+++ social-alpha/src/components/rightSide/RightSide.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
-
+import Home from '../../img/home.png'
+import Noti from '../../img/noti.png'
+import Comment from '../../img/comment.png'
+import {UilSetting } from '@iconscout/react-unicons'
 
 const RightSide = () => {
   return (
-<div className="RightSide">
-    Righrtt Side
+<div className="RightSide flex flex-col gap-[rem]">
+  <div className="navIcons flex mt-[1rem] justify-between">
+    <img src={Home} className="w-[1.5rem] h-[1.5rem]"/>
+    <UilSetting/>
+    <img src={Noti} className="w-[1.5rem] h-[1.5rem]"/>
+    <img src={Comment} className="w-[1.5rem] h-[1.5rem]"/>
+
+  </div>
 </div>  )
 }
 
