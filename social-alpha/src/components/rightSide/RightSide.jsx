8a8e8fbff54b2eb3dff6770f60d47a741b75d794
Patryk <patrykokpara@hotmail.com>
2023-05-17 12:42:17 +0100

    Starting trend card

--- social-alpha/src/components/rightSide/RightSide.jsx
+++ social-alpha/src/components/rightSide/RightSide.jsx
@@ -3,6 +3,7 @@ import Home from '../../img/home.png'
 import Noti from '../../img/noti.png'
 import Comment from '../../img/comment.png'
 import {UilSetting } from '@iconscout/react-unicons'
+import TrendCard from '../trendCard/TrendCard'
 
 const RightSide = () => {
   return (
@@ -12,6 +13,8 @@ const RightSide = () => {
     <UilSetting/>
     <img src={Noti} className="w-[1.5rem] h-[1.5rem]"/>
     <img src={Comment} className="w-[1.5rem] h-[1.5rem]"/>
+
+    <TrendCard/>
 
   </div>
 </div>  )
