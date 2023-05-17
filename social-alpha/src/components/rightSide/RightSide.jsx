2f47e8b245855ced874bf583148fee190212ad4d
Patryk <patrykokpara@hotmail.com>
2023-05-17 13:16:41 +0100

    Finished trends part

--- social-alpha/src/components/rightSide/RightSide.jsx
+++ social-alpha/src/components/rightSide/RightSide.jsx
@@ -1,23 +1,22 @@
-import React from 'react'
-import Home from '../../img/home.png'
-import Noti from '../../img/noti.png'
-import Comment from '../../img/comment.png'
-import {UilSetting } from '@iconscout/react-unicons'
-import TrendCard from '../trendCard/TrendCard'
+import React from "react";
+import Home from "../../img/home.png";
+import Noti from "../../img/noti.png";
+import Comment from "../../img/comment.png";
+import { UilSetting } from "@iconscout/react-unicons";
+import TrendCard from "../trendCard/TrendCard";
 
 const RightSide = () => {
   return (
-<div className="RightSide flex flex-col gap-[rem]">
-  <div className="navIcons flex mt-[1rem] justify-between">
-    <img src={Home} className="w-[1.5rem] h-[1.5rem]"/>
-    <UilSetting/>
-    <img src={Noti} className="w-[1.5rem] h-[1.5rem]"/>
-    <img src={Comment} className="w-[1.5rem] h-[1.5rem]"/>
+    <div className="RightSide flex flex-col gap-[2rem]">
+      <div className="navIcons flex mt-[1rem] justify-between">
+        <img src={Home} className="w-[1.5rem] h-[1.5rem]" />
+        <UilSetting />
+        <img src={Noti} className="w-[1.5rem] h-[1.5rem]" />
+        <img src={Comment} className="w-[1.5rem] h-[1.5rem]" />
+      </div>
+      <TrendCard />
+    </div>
+  );
+};
 
-    <TrendCard/>
-
-  </div>
-</div>  )
-}
-
-export default RightSide
+export default RightSide;
--- social-alpha/src/components/trendCard/TrendCard.jsx
+++ social-alpha/src/components/trendCard/TrendCard.jsx
@@ -1,24 +1,20 @@
 import React from "react";
-import {TrendData} from "../Data/TrendData.js"
+import { TrendData } from "../Data/TrendData.js";
 
 const TrendCard = () => {
-  return <div className="TrendCard">
-
-
-    <h3>Trends for you</h3> 
-    {TrendData.map((trend) => {
-      return(
-        <div className="trend">
-          <span>
-          #{trend.name}
-          </span>
-          <span>
-            {trend.shares}k shares
-          </span>
-        </div>
-      )
-    })}
-  </div>
+  return (
+    <div className="TrendCard flex flex-col gap-[1rem] bg-[rgba(255,255,255,0.64)] p-[1rem] rounded-[1rem] pl-[2rem]">
+      <h3 className=" font-bold">Trends for you</h3>
+      {TrendData.map((trend) => {
+        return (
+          <div className="trend flex flex-col gap-[0.5rem]">
+            <span className="font-bold  ">#{trend.name}</span>
+            <span className="font-[13px]  ">{trend.shares}k shares</span>
+          </div>
+        )
+      })}
+    </div>
+  );
 };
 
 export default TrendCard;
