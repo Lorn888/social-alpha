a4b91fcee455cc385207653a38892336396854cf
Patryk <patrykokpara@hotmail.com>
2023-05-17 12:51:19 +0100

    Added fake trend data

--- social-alpha/src/components/trendCard/TrendCard.jsx
+++ social-alpha/src/components/trendCard/TrendCard.jsx
@@ -1,8 +1,23 @@
 import React from "react";
+import {TrendData} from "../Data/TrendData.js"
 
 const TrendCard = () => {
   return <div className="TrendCard">
-    trend 
+
+
+    <h3>Trends for you</h3> 
+    {TrendData.map((trend) => {
+      return(
+        <div className="trend">
+          <span>
+          #{trend.name}
+          </span>
+          <span>
+            {trend.shares}k shares
+          </span>
+        </div>
+      )
+    })}
   </div>
 };
 
